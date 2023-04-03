d7dac7d46347d2040f87841e51594c41359ec168
LayZNimrod <2zachmiller@gmail.com>
2023-04-03 13:52:18 -0400

    fixed it and added more physics to lemon

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ var cursors;
 //velocity[0] is velocity x and velocity[1] is velocity y
 var velocity = [0,0];
 var velocityCap=600;
-var rotaionL=false;
+var rotationL=false;
 
 
 var game = new Phaser.Game(config);
@@ -41,7 +41,7 @@ function preload ()
   this.load.image('sky', 'images/backround.jpg');
   
   //load spritesheet
-  this.load.spritesheet('dude', 
+  this.load.spritesheet('lemon', 
   'images/Princess_Lemon-Sheet.png',
   { frameWidth: 60, frameHeight: 96 });
 }
@@ -56,67 +56,67 @@ function create ()
   platforms.create(950, 700, 'ground').setScale(10).refreshBody();
 
   //player create
-  player = this.physics.add.sprite(100, 350, 'dude');
+  player = this.physics.add.sprite(100, 350, 'lemon');
 
   
   player.setCollideWorldBounds(true);
 
   this.anims.create({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 9, end: 12 }),
+    frames: this.anims.generateFrameNumbers('lemon', { start: 9, end: 12 }),
     frameRate: 10,
   });
 
   this.anims.create({
     key: 'turnR',
-    frames: [ { key: 'dude', frame: 0 } ],
+    frames: [ { key: 'lemon', frame: 0 } ],
     frameRate: 1
   });
 
   this.anims.create({
     key: 'turnL',
-    frames: [ { key: 'dude', frame: 8 } ],
+    frames: [ { key: 'lemon', frame: 8 } ],
     frameRate: 1
   });
 
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
+    frames: this.anims.generateFrameNumbers('lemon', { start: 4, end: 7 }),
     frameRate: 10
   });
   this.anims.create({
     key: 'jump1R',
-    frames: [ { key: 'dude', frame: 1 } ],
-    frameRate: 1
+    frames: this.anims.generateFrameNumbers('lemon', { start: 1, end: 3 }),
+    frameRate: 10
   });
 
   this.anims.create({
     key: 'jump2R',
-    frames: [ { key: 'dude', frame: 2 } ],
+    frames: [ { key: 'lemon', frame: 2 } ],
     frameRate: 1
   });
 
   this.anims.create({
     key: 'jump3R',
-    frames: [ { key: 'dude', frame: 3 } ],
+    frames: [ { key: 'lemon', frame: 3 } ],
     frameRate: 1
   });
 
   this.anims.create({
     key: 'jump1L',
-    frames: [ { key: 'dude', frame: 8 } ],
-    frameRate: 1
+    frames: this.anims.generateFrameNumbers('lemon', { start: 14, end: 16 }),
+    frameRate: 10
   });
 
   this.anims.create({
     key: 'jump2L',
-    frames: [ { key: 'dude', frame: 9 } ],
+    frames: [ { key: 'lemon', frame: 6 } ],
     frameRate: 1
   });
 
   this.anims.create({
     key: 'jump3L',
-    frames: [ { key: 'dude', frame: 10 } ],
+    frames: [ { key: 'lemon', frame: 7 } ],
     frameRate: 1
   });
 
@@ -124,7 +124,7 @@ function create ()
   //for actual animations
   // this.anims.create({
   //   key: 'right',
-  //   frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+  //   frames: this.anims.generateFrameNumbers('lemon', { start: 5, end: 8 }),
   //   frameRate: 10,
   //   repeat: -1
   // });
@@ -142,35 +142,50 @@ function update ()
   if (player.body.touching.down)
   {
     velocityCap=600;
+    velocity[1]=0;
   }
 
-  if (cursors.cursors.left.isDown && !player.body.touching.down)
+  if (cursors.left.isDown && cursors.right.isDown && player.body.touching.down)
+  {
+    velocity[0]*=.7;
+    player.setVelocityX(velocity[0]);
+  }
+  else if (cursors.left.isDown && player.body.touching.down)
   {
     velocity[0]-=100;
     player.setVelocityX(velocity[0]);
   }
-  else if (cursors.left.isDown)
-  {
-    player.anims.play('left', true);
-  }
-  else if (cursors.right.isDown)
+  else if (cursors.right.isDown && player.body.touching.down)
   {
     velocity[0]+=100;
     player.setVelocityX(velocity[0]);
-
-    player.anims.play('right', true);
+  } 
+  else if (cursors.left.isDown && cursors.right.isDown && player.body.touching.down===false)
+  {
+    velocity[0]*=.95;
+    player.setVelocityX(velocity[0]);
+  }
+  else if (cursors.left.isDown && player.body.touching.down===false)
+  {
+    velocity[0]-=50;
+    player.setVelocityX(velocity[0]);
+  }
+  else if (cursors.right.isDown && player.body.touching.down===false)
+  {
+    velocity[0]+=50;
+    player.setVelocityX(velocity[0]);
   } 
   else if (!player.body.touching.down)
   {
     velocity[0]*=.95;
     player.setVelocityX(velocity[0]);
   } 
-  //jump anim
   else
   {
     velocity[0]*=.7;
     player.setVelocityX(velocity[0]);
 
+    //scrapped
     // if (rotationL==true)
     // {
     //   player.anims.play('turnL');
@@ -185,16 +200,20 @@ function update ()
   {
     velocity[1]-=800;
     player.setVelocityY(velocity[1]);
-    player.anims.play('jump1L');
   }
-
-  if (keyD.isDown&&player.body.touching.down)
+  else if (keyD.isDown&&player.body.touching.down)
   {
     //todo add animation
     velocity[1]-=600;
     player.setVelocityY(velocity[1]);
-
     velocityCap=900;
+    if (rotationL===true){
+      velocity[0]=- 900;
+    } 
+    else if (rotationL===false)
+    {
+      velocity[0]=900;
+    }
   }
 
   //velocity capps
@@ -216,5 +235,32 @@ function update ()
   else if(velocity[0]>0.1)
   {
     rotationL=false;
+  }
+
+
+  //animations
+  if(player.body.touching.down===true && velocity[0]<-0.1)
+  {
+    player.anims.play('left',true);
+  }
+  else if(player.body.touching.down===true && velocity[0]>0.1)
+  {
+    player.anims.play('right',true);
+  }
+  else if(player.body.touching.down===true && rotationL===true)
+  {
+    player.anims.play('turnL');
+  }
+  else if(player.body.touching.down===true && rotationL===false)
+  {
+    player.anims.play('turnR');
+  }
+  else if(player.body.touching.down===false && rotationL===true)
+  {
+    player.anims.play('jump1L',true);
+  }
+  else if(player.body.touching.down===false && rotationL===false)
+  {
+    player.anims.play('jump1R',true);
   }
 }
