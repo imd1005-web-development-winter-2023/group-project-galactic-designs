aa6fb0d8b9045f1a4ae2222de3044ecae6d2a1df
NurpoHB <102769423+NurpoHB@users.noreply.github.com>
2023-04-11 00:55:15 -0400

    new tiles thingy

--- json/tiles.tsx
+++ json/tiles.tsx
@@ -1,7 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="64" tileheight="64" tilecount="37" columns="37">
- <editorsettings>
-  <export target="Tiles/tiles.json" format="json"/>
- </editorsettings>
  <image source="Tiles/tiles.png" width="2368" height="64"/>
 </tileset>
