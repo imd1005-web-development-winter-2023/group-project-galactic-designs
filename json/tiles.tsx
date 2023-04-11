646840b5987cb70416dabd14a8d9cdf1f9fd50c5
NurpoHB <102769423+NurpoHB@users.noreply.github.com>
2023-04-11 00:32:55 -0400

    Reuploaidng files part 2

--- json/tiles.tsx
+++ json/tiles.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="64" tileheight="64" tilecount="37" columns="37">
- <image source="tiles.png" width="2368" height="64"/>
+ <editorsettings>
+  <export target="Tiles/tiles.json" format="json"/>
+ </editorsettings>
+ <image source="Tiles/tiles.png" width="2368" height="64"/>
 </tileset>
