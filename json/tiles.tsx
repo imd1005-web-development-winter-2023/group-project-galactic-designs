970558fb2fe233ed112bfcc37238ce364eb0ad2b
NurpoHB <102769423+NurpoHB@users.noreply.github.com>
2023-04-10 23:05:49 -0400

    WRONG FILES

--- json/tiles.tsx
+++ json/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="16" tileheight="16" tilecount="37" columns="37">
- <image source="Tiles/tiles.png" width="592" height="16"/>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="128" tileheight="128" tilecount="37" columns="37">
+ <image source="tiles.png" width="4736" height="128"/>
 </tileset>
