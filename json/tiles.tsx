37c5ba6fdc2d3f9801e95913c05f3dd9ade450a4
NurpoHB <102769423+NurpoHB@users.noreply.github.com>
2023-04-11 03:08:39 -0400

    Unfuck it up maybe?

--- json/tiles.tsx
+++ json/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="64" tileheight="64" tilecount="37" columns="37">
- <image source="Tiles/tiles.png" width="2368" height="64"/>
+ <image source="tiles.png" width="2368" height="64"/>
 </tileset>
